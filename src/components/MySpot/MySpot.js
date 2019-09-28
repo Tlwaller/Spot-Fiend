131f8de10572d3ebce219db649acba970b01c015
Trey Waller <Trey@treys-mbp.attlocal.net>
2019-09-28 18:29:38 -0500

    converted individual posts to a seperate component

---
+++ src/components/MySpot/MySpot.js
@@ -0,0 +1,104 @@
+import {connect} from 'react-redux';
+import {updateUserPosts, deletePost, editPost} from '../../ducks/reducers/postsReducer'
+import React, { Component } from 'react'
+import './MySpot.css';
+
+class MySpot extends Component {
+    constructor() {
+        super();
+        this.state = {
+                title: '',
+                address: '',
+                description: '',
+                editing: false
+        }
+    }
+
+    componentDidMount() {
+        this.props.updateUserPosts();
+    }
+
+    handleTitle = e => {
+        this.setState({title: e.target.value})
+    }
+
+    handleDelete = (spot_id) => {
+        this.props.deletePost(spot_id);
+    }
+
+    handleEdit = (spot_id) => {
+        
+        const {title, address, description} = this.state;
+        console.log("myspotlist state: " + spot_id, title, address, description)
+        this.props.editPost({spot_id, title, address, description})
+    }
+
+    toggleEdit = () => {
+        if(this.state.editing === false){
+            this.setState({editing: true})
+        } else this.setState({editing: false})
+    }
+
+    handleInput = e => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    render() {
+        const {post, i} = this.props;
+            return (
+                <div key={i} className='my-post'>
+                    <div className='my-post-item-container'>
+                        <h2 className='my-post-item'>{post.title}</h2>
+                        <p className='my-post-item'>{post.description}</p>
+                        <h5 className='my-post-item'>{post.address}</h5>
+                    </div>
+
+                    <div className='mypost-btn-container'>
+                            <img src='https://www.shareicon.net/data/128x128/2016/09/05/825547_document_512x512.png'onClick={this.toggleEdit} className='toggle-edit'/>
+                            <img src='https://www.shareicon.net/data/128x128/2016/09/05/825117_delete_512x512.png'
+                            className='delet-btn'
+                            onClick={() => this.handleDelete(post.spot_id)}
+                            />
+                        </div>
+
+                    <form className='edit-post-form' style={this.state.editing? {display: 'flex'} : {display: 'none'}}>
+                        <input
+                        className='edit-post-input'
+                            type='text'
+                            placeholder='edit title'
+                            name='title'
+                            onChange={this.handleInput}
+                        />
+                        <input
+                        className='edit-post-input'
+                            type='text'
+                            placeholder='edit address'
+                            name='address'
+                            onChange={this.handleInput}
+                        />
+                        <input
+                        className='edit-post-input'
+                            type='text'
+                            placeholder='edit description'
+                            name='description'
+                            onChange={this.handleInput}
+                        />
+                        
+                            <button
+                                className='edit-btn'
+                                onClick={() => this.handleEdit(post.spot_id)}
+                            >Submit</button>
+                    </form>
+                </div>
+            )
+    }
+}
+
+const mapStateToProps = reduxState => {
+    return {
+        posts: reduxState.postsReducer.posts,
+        userId: reduxState.userReducer.userId
+    }
+}
+
+export default connect(mapStateToProps, {updateUserPosts, deletePost, editPost})(MySpot);
--- src/components/MySpotList/MySpotList.js
+++ src/components/MySpotList/MySpotList.js
@@ -1,98 +1,22 @@
 import React, {Component} from 'react';
 import './MySpotList.css';
 import Nav from '../Nav/Nav';
+import MySpot from '../MySpot/MySpot';
 import {connect} from 'react-redux';
-import {updateUserPosts, deletePost, editPost} from '../../ducks/reducers/postsReducer'
+import {updateUserPosts} from '../../ducks/reducers/postsReducer'
 
 class MySpotList extends Component {
-    constructor() {
-        super();
-        this.state = {
-                title: '',
-                address: '',
-                description: '',
-                editing: false
-        }
-    }
 
     componentDidMount() {
         this.props.updateUserPosts();
     }
 
-    handleTitle = e => {
-        this.setState({title: e.target.value})
-    }
-
-    handleDelete = (spot_id) => {
-        this.props.deletePost(spot_id);
-    }
-
-    handleEdit = (spot_id) => {
-        const {title, address, description} = this.state;
-        console.log("myspotlist state: " + spot_id, title, address, description)
-        this.props.editPost({spot_id, title, address, description})
-    }
-
-    toggleEdit = () => {
-        if(this.state.editing === false){
-            this.setState({editing: true})
-        } else this.setState({editing: false})
-    }
-
-    handleInput = e => {
-        this.setState({[e.target.name]: e.target.value});
-    }
-
     render() {
         const postsMapped = this.props.posts.map((post, i) => {
-            return (
-                <div key={i} className='my-post'>
-                    <div className='my-post-item-container'>
-                        <h2 className='my-post-item'>{post.title}</h2>
-                        <p className='my-post-item'>{post.description}</p>
-                        <h5 className='my-post-item'>{post.address}</h5>
-                    </div>
-
-                    <button onClick={this.toggleEdit} className='toggle-edit'>{this.state.editing? 'Back' : 'Edit'}</button>
-
-                    <form className='edit-post-form' style={this.state.editing? {display: 'flex'} : {display: 'none'}}>
-                        <input
-                        className='edit-post-input'
-                            type='text'
-                            placeholder='edit title'
-                            name='title'
-                            onChange={this.handleInput}
-                        />
-                        <input
-                        className='edit-post-input'
-                            type='text'
-                            placeholder='edit address'
-                            name='address'
-                            onChange={this.handleInput}
-                        />
-                        <input
-                        className='edit-post-input'
-                            type='text'
-                            placeholder='edit description'
-                            name='description'
-                            onChange={this.handleInput}
-                        />
-                        <button
-                            className='edit-btn'
-                            onClick={() => this.handleEdit(post.spot_id)}
-                        >Submit</button>
-                    </form>
-                    
-                    
-
-                    <button className='delet-btn'
-                        onClick={() => this.handleDelete(post.spot_id)}
-                    >Remove</button> 
-                </div>
-            )
+            return <MySpot post={post} i={i}/>
         })
 
-        if(postsMapped.length === 0) {
+        if (postsMapped.length === 0) {
             return (
                 <div id='my-spots-page'>
                 <Nav/>
@@ -103,6 +27,7 @@ class MySpotList extends Component {
             </div>
             )
         }
+
         return (
             <div id='my-spots-page'>
                 <Nav/>
@@ -112,7 +37,9 @@ class MySpotList extends Component {
                 </div>
             </div>
         )
+
     }
+
 }
 
 const mapStateToProps = reduxState => {
@@ -122,4 +49,4 @@ const mapStateToProps = reduxState => {
     }
 }
 
-export default connect(mapStateToProps, {updateUserPosts, deletePost, editPost})(MySpotList);
+export default connect(mapStateToProps, {updateUserPosts})(MySpotList);
