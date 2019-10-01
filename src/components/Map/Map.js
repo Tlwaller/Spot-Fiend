98cc2803ac8e13c3f56a1c91cd847e5dd8d2e96a
Trey Waller <Trey@Treys-MacBook-Pro.local>
2019-09-30 23:12:10 -0500

    google maps successfully implemented

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,35 +1,54 @@
-import React, { Component } from 'react'
-import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps';
+import React, {Component, useState} from 'react'
+import {GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow} from 'react-google-maps';
 import {updatePosts} from '../../ducks/reducers/postsReducer';
 import {connect} from 'react-redux';
+import mapStyles from './MapStyles';
 
-class Map extends Component {
-    componentDidMount() {
-        this.props.updatePosts()
-    }
+function Map(props) {
+    const [selectedSpot, setSelectedSpot] = useState(null);
 
-    render() {
-        return (
-            <div>
-                <GoogleMap 
-                defaultZoom={10}
-                defaultCenter={{lat: 32.776665, lng: -96.796989}}
-                >
-                    {this.props.spots.map(spot => {
-                        const latitude = parseFloat(spot.latitude);
-                        const longitude = parseFloat(spot.longitude);
-                        return <Marker 
-                        key={spot.spot_id} 
-                        position={{
-                            lat: (latitude),
-                            lng: (longitude)
-                        }}
-                        />
-                    })}
-            </GoogleMap>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <GoogleMap 
+            defaultZoom={10}
+            defaultCenter={{lat: 32.776665, lng: -96.796989}}
+            defaultOptions={{styles: mapStyles}}
+            >
+                {props.spots.map(spot => {
+                    const latitude = parseFloat(spot.latitude);
+                    const longitude = parseFloat(spot.longitude);
+                    return <Marker 
+                    key={spot.spot_id} 
+                    position={{
+                        lat: (latitude),
+                        lng: (longitude)
+                    }}
+                    onClick={() => {
+                        console.log(spot)
+                        setSelectedSpot(spot);
+                    }}
+                    />
+                })}
+
+                {selectedSpot && (
+                    <InfoWindow 
+                    position={{
+                        lat: (parseFloat(selectedSpot.latitude)),
+                        lng: (parseFloat(selectedSpot.longitude))
+                    }}
+                    onCloseClick={() => {
+                        setSelectedSpot(null);
+                    }}
+                    >
+                        <div>
+                        <h3>{selectedSpot.title}</h3>
+                        <p>{selectedSpot.description}</p>
+                        </div>
+                    </InfoWindow>
+                )}
+        </GoogleMap>
+        </div>
+    )
 }
 
 const mapStateToProps = reduxState => {
---
+++ src/components/Map/MapStyles.js
@@ -0,0 +1,234 @@
+export default
+[
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "saturation": 36
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 40
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 17
+            },
+            {
+                "weight": 1.2
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.province",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#979797"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ece9e9"
+            },
+            {
+                "visibility": "on"
+            },
+            {
+                "weight": "7.99"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#545454"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.neighborhood",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#b9b6b6"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 20
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 21
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffcc00"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "lightness": 29
+            },
+            {
+                "weight": 0.2
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffcc30"
+            },
+            {
+                "lightness": 18
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#ffcc40"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 19
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#bdbdbd"
+            },
+            {
+                "lightness": 17
+            }
+        ]
+    }
+]
